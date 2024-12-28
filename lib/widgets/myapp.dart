c7c1984d803afd614185913d93c16321aecec116
Mohammed Tohamy <125455529+mohammedtohamym@users.noreply.github.com>
2024-12-28 17:54:16 +0200

    added snackbar

--- lib/widgets/myapp.dart
+++ lib/widgets/myapp.dart
@@ -62,60 +62,82 @@ class _MyAppState extends State<MyApp> {
                   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                       crossAxisCount: 2),
                   children: [
-                    Container(
-                      child: Stack(
-                        alignment: Alignment.bottomLeft,
-                        children: [
-                          Image.network(
-                            'https://images.unsplash.com/photo-1484788984921-03950022c9ef?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wfGVufDB8fDB8fHww',
-                            fit: BoxFit.cover,
-                            height: double.infinity,
-                            width: double.infinity,
-                          ),
-                          Container(
-                            padding: EdgeInsets.all(5),
-                            color: const Color.fromARGB(255, 17, 156, 255),
-                            child: Row(
-                              mainAxisSize:const MainAxisSize.min,
-                              children: const [
-                                Padding(
-                                  padding: EdgeInsets.all(8.0),
-                                  child: Text('Laptop 1'),
-                                ),
-                                Icon(Icons.shop)
-                              ],
+                    Builder(builder: (context) {
+                      return Container(
+                        child: Stack(
+                          alignment: Alignment.bottomLeft,
+                          children: [
+                            Image.network(
+                              'https://images.unsplash.com/photo-1484788984921-03950022c9ef?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wfGVufDB8fDB8fHww',
+                              fit: BoxFit.cover,
+                              height: double.infinity,
+                              width: double.infinity,
                             ),
-                          )
-                        ],
-                      ),
-                    ),
-                    Container(
-                      child: Stack(
-                        alignment: Alignment.bottomLeft,
-                        children: [
-                          Image.network(
-                            'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-                            fit: BoxFit.cover,
-                            height: double.infinity,
-                            width: double.infinity,
-                          ),
-                          Container(
-                            padding: EdgeInsets.all(5),
-                            color: const Color.fromARGB(255, 17, 156, 255),
-                            child: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text('Laptop 2'),
-                                ),
-                                Icon(Icons.shop)
-                              ],
+                            Container(
+                              padding: EdgeInsets.all(5),
+                              color: const Color.fromARGB(255, 17, 156, 255),
+                              child: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.all(8.0),
+                                    child: Text('Laptop 1'),
+                                  ),
+                                  InkWell(
+                                      onTap: () {
+                                        final snackBar = SnackBar(
+                                          content:
+                                              const Text('Item added to cart!'),
+                                        );
+                                        ScaffoldMessenger.of(context)
+                                            .showSnackBar(snackBar);
+                                      },
+                                      child: Icon(Icons.shop))
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      );
+                    }),
+                    Builder(builder: (context) {
+                      return Container(
+                        child: Stack(
+                          alignment: Alignment.bottomLeft,
+                          children: [
+                            Image.network(
+                              'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                              fit: BoxFit.cover,
+                              height: double.infinity,
+                              width: double.infinity,
                             ),
-                          )
-                        ],
-                      ),
-                    ),
+                            Container(
+                              padding: EdgeInsets.all(5),
+                              color: const Color.fromARGB(255, 17, 156, 255),
+                              child: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text('Laptop 2'),
+                                  ),
+                                  InkWell(
+                                      onTap: () {
+                                        final snackBar = SnackBar(
+                                          content:
+                                              const Text('Item added to cart!'),
+                                        );
+                                        ScaffoldMessenger.of(context)
+                                            .showSnackBar(snackBar);
+                                      },
+                                      child: Icon(Icons.shop))
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      );
+                    }),
                   ],
                 ),
               ),
